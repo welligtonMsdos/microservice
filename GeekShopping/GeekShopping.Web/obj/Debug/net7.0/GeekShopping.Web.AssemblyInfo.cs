059c3081ad96cce1f0d5c8580543f8192ac32632
Dell <Dell@DESKTOP-TK7GSO1>
2023-12-05 20:17:34 -0300

    feature: crud Product

--- GeekShopping/GeekShopping.Web/obj/Debug/net7.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping/GeekShopping.Web/obj/Debug/net7.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aaa5819fbd25ce62d9ec8d3e58b5ac007a897d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
