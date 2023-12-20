e82523902f5d3524f8b935b4757363c8c3214309
Dell <Dell@DESKTOP-TK7GSO1>
2023-12-20 00:35:46 -0300

    style: product screen redesign

--- GeekShopping/GeekShopping.Web/Models/Product.cs
+++ GeekShopping/GeekShopping.Web/Models/Product.cs
@@ -1,11 +1,23 @@
-﻿namespace GeekShopping.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.Web.Models;
 
 public class Product
 {
     public long Id { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
     public decimal Price { get; set; }
+
+    [Required]
     public string Description { get; set; }
+
+    [Required]   
     public string CategoryName { get; set; }
+
+    [Required]
     public string ImageURL { get; set; }
 }
