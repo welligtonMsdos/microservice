58ce026b184ac2edbd92cc74967407608acb5d59
Dell <Dell@DESKTOP-TK7GSO1>
2023-12-21 01:24:56 -0300

    feat: product listing and details screen

--- GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +8,29 @@ namespace GeekShopping.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;     
+        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
-            _logger = logger;            
+            _logger = logger;
+            _productService = productService;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }       
+            var products = await _productService.FindAllProducts();
+
+            return View(products);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int id)
+        {
+            var model = await _productService.FindProductById(id);
+
+            return View(model);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- GeekShopping/GeekShopping.Web/Models/Product.cs
+++ GeekShopping/GeekShopping.Web/Models/Product.cs
@@ -20,4 +20,21 @@ public class Product
 
     [Required]
     public string ImageURL { get; set; }
+
+    [Range(1,100)]
+    public int Count { get; set; } = 1;
+
+    public string SubstringName()
+    {
+        if (Name.Length < 24) return Name;
+
+        return $"{Name.Substring(0, 21)} ...";
+    }
+
+    public string SubstringDescription()
+    {
+        if (Description.Length < 355) return Description;
+
+        return $"{Description.Substring(0, 352)} ...";
+    }
 }
